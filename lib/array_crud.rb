424d0d39e876cda5c35c7827e21bdcf4a32cd675
Molly Dolan <dolanmollye@gmail.com>
2020-11-01 21:04:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  new_array = remove_element_from_end_of_array.pop
+  p new array
 end
 
 def remove_element_from_start_of_array(array)
