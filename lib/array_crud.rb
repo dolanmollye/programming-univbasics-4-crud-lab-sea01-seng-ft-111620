de7b41b34aa4d7e0d62a477e5002894dd64152f4
Molly Dolan <dolanmollye@gmail.com>
2020-11-01 20:53:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  [purple, green, blue]
+  add_element_to_end_of_array << orange
 end
 
 def add_element_to_start_of_array(array, element)
