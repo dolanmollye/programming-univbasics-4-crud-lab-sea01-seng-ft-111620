ff0716e5bcffe635bf8d482962df382dfa0ef173
Molly Dolan <dolanmollye@gmail.com>
2020-11-01 20:59:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = [array]
-  remove_element_from_end_of_array.pop
+  remove_element_from_end_of_array.pop ()
   p remove_element_from_end_of_array
 end
 
