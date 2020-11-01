88f4bb48bdfcdf694bd182eb2629b058839ac4c9
Molly Dolan <dolanmollye@gmail.com>
2020-11-01 20:55:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   [array]
   add_element_to_end_of_array << element
-  
+  p add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
